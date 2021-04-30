bf2e36d9899ee457e06818222b48f775768fb7d8
Lukas Kotlik <kotliluk@fel.cvut.cz>
2021-04-30 17:46:57 +0200

    Fix typos in application messages.

--- src/language/en.ts
+++ src/language/en.ts
@@ -8,16 +8,16 @@ import {SyntaxErrorMessages} from "../error/raSyntaxError";
  */
 const codeErrors: CodeErrorMessages = {
     resultSection_nodeIndexNotFound:
-        ["ResultSection.getCurrentRelation: Selected node index in evaluation tree not found."],
+        ["ResultSection.getCurrentRelation: Selected node index in the evaluation tree was not found."],
     resultSection_evalError:
         ["ResultSection.getCurrentRelation: Evaluation error in result section: ", /* error */ "."],
     resultSection_nullRelationToSave:
-        ["ResultSection.saveResultRelation: Result relation is null when tried to save it."],
+        ["ResultSection.saveResultRelation: Resulting relation is null when trying to save it."],
     resultSection_nullRelationToAdd:
-        ["ResultSection.handleAddRelation: Result relation is null when tried to add it."],
+        ["ResultSection.handleAddRelation: Resulting relation is null when trying to add it."],
 
     row_absentColumn:
-        ["Row.getOrderedValues: Column ", /* column */ " is absent in row with columns: ", /* column */ "."],
+        ["Row.getOrderedValues: Column ", /* column */ " is missing in a row with columns: ", /* column */ "."],
 
     valueParser_unexpectedToken:
         ["ValueParser.rpnToVETreeRecursive: Unexpected token: ", /* token */ "."],
@@ -34,21 +34,21 @@ const codeErrors: CodeErrorMessages = {
  * English messages for RASemanticErrors mapped by their error codes.
  */
 const semanticErrors: SemanticErrorMessages = {
-    exprParser_relationNotDefined: ["Relation \"", /* relation */ "\" is not defined. Check relations definitions."],
+    exprParser_relationNotDefined: ["Relation \"", /* relation */ "\" is not defined. Check definitions of relations."],
 
     binaryNode_commonColumns: ["Source relations for ", /* operator */ " have common columns \"", /* columns */ "\"."],
     setOperationNode_notEqualColumns:
         ["Source relations \"", /* left */ "\" and \"", /* right */ "\" for set ", /* operation */ " do not have the same column sets."],
     divisionNode_rightColumnsNotSubset:
-        ["Right source relation schema \"", /* schema */ "\" is not a subset of the left source relation schema \"", /* schema */ "\"."],
+        ["Right-hand relation schema \"", /* schema */ "\" is not a subset of the left-hand relation schema \"", /* schema */ "\"."],
     divisionNode_rightColumnsNotProperSubset:
-        ["Right source relation schema \"", /* schema */ "\" is not a proper subset of the left source relation schema \"",
-        /* schema */ ". There must exist a column in the left relation which is not present in the right relation."],
-    renameNode_absentOriginalColumn: ["Invalid rename, column \"", /* column */ "\" does not exist in the source relation."],
-    renameNode_changeToDuplicit: ["Invalid rename, new column name \"", /* column */ "\" is duplicit in the changed relation."],
+        ["Right-hand relation schema \"", /* schema */ "\" is not a proper subset of the left-hand relation schema \"",
+        /* schema */ "\". There must exist at least one column in the left relation which is not present in the right relation."],
+    renameNode_absentOriginalColumn: ["Invalid renaming attempt, column \"", /* column */ "\" does not exist in the source relation."],
+    renameNode_changeToDuplicit: ["Invalid renaming attempt, new column name \"", /* column */ "\" is ambiguous in the changed relation."],
     projectionNode_absentColumn: ["Invalid projection of the column \"", /* column */ "\". It does not exist in the source relation."],
 
-    referenceValue_absentColumn: ["Column \"", /* column */ "\" is absent in a schema with columns: ", /* columns */ "."]
+    referenceValue_absentColumn: ["Column \"", /* column */ "\" is missing in a schema with columns: ", /* columns */ "."]
 };
 
 /**
@@ -57,13 +57,13 @@ const semanticErrors: SemanticErrorMessages = {
 const syntaxErrors: SyntaxErrorMessages = {
     exprParser_emptyStringGiven: ["Cannot parse an expression from an empty string."],
     exprParser_outerJoinWhenNullNotSupported: ["Found ", /* outer join */ " when null values are not supported."],
-    exprParser_unexpectedPart: ["Unexpected part \"", /* part */ "\" in RA expression."],
+    exprParser_unexpectedPart: ["Unexpected part \"", /* part */ "\" in an expression."],
     exprParser_bothBranchesError: ["All combinations throw error in \"", /* part */ "\". When treated as projection:\n",
         /* error */ "\nWhen treated as theta join:\n", /* error */ ""],
-    exprParser_invalidExpression: ["Given string is not a valid expression."],
+    exprParser_invalidExpression: ["Given string is not a valid relational algebra expression."],
     exprParser_invalidParentheses: ["Invalid structure of parentheses in the expression."],
-    exprParser_invalidStart: ["RA expression cannot start with ", /* start */ "."],
-    exprParser_invalidEnd: ["RA expression cannot end with ", /* end */ "."],
+    exprParser_invalidStart: ["Relational algebra expression cannot start with ", /* start */ "."],
+    exprParser_invalidEnd: ["Relational algebra expression cannot end with ", /* end */ "."],
     exprParser_relationAfterRelation: ["Relation \"", /* relation */ "\" after relation \"", /* relation */"\"."],
     exprParser_relationAfterUnary: ["Relation \"", /* relation */ "\" after unary operator \"", /* unary */ "\"."],
     exprParser_relationAfterClosing: ["Relation \"", /* relation */ "\" after closing parenthesis."],
@@ -71,15 +71,15 @@ const syntaxErrors: SyntaxErrorMessages = {
     exprParser_unaryAfterOpening: ["Unary operator \"", /* unary */ "\" after opening parenthesis."],
     exprParser_binaryAfterBinary: ["Binary operator \"", /* unary */ "\" after binary operator \"", /* binary */ "\"."],
     exprParser_binaryAfterOpening: ["Binary operator \"", /* binary */ "\" after opening parenthesis."],
-    exprParser_openingAfterRelation: ["Opening parentheses after relation \"", /* relation */ "\"."],
-    exprParser_openingAfterUnary: ["Opening parentheses after unary operator \"", /* unary */ "\"."],
-    exprParser_openingAfterClosing: ["Opening parentheses after closing parentheses."],
-    exprParser_closingAfterBinary: ["Closing parentheses after binary operator \"", /* binary */ "\"."],
-    exprParser_closingAfterOpening: ["Closing parentheses after opening parentheses."],
+    exprParser_openingAfterRelation: ["Opening parenthesis after relation \"", /* relation */ "\"."],
+    exprParser_openingAfterUnary: ["Opening parenthesis after unary operator \"", /* unary */ "\"."],
+    exprParser_openingAfterClosing: ["Opening parenthesis after closing parenthesis."],
+    exprParser_closingAfterBinary: ["Closing parenthesis after binary operator \"", /* binary */ "\"."],
+    exprParser_closingAfterOpening: ["Closing parenthesis after opening parenthesis."],
 
-    valueParser_emptyInput: ["An empty string given as a condition."],
+    valueParser_emptyInput: ["Empty string given as a condition."],
     valueParser_unsupportedNull: ["Null constant used when null values are not supported."],
-    valueParser_unexpectedPart: ["Unexpected part \"", /* part */ "\" in expression."],
+    valueParser_unexpectedPart: ["Unexpected part \"", /* part */ "\" in the expression."],
     valueParser_missingOpeningParenthesis: ["Missing opening parenthesis '(' in an expression."],
     valueParser_missingClosingParenthesis: ["Missing closing parenthesis ')' in an expression."],
     valueParser_invalidExpression: ["Given string is not a valid expression."],
@@ -87,37 +87,37 @@ const syntaxErrors: SyntaxErrorMessages = {
     valueParser_invalidEnd: ["Expression cannot end with \"", /* end */ "\"."],
     valueParser_literalAfterLiteral: ["Literal \"", /* literal */ "\" after literal \"", /* literal */ "\"."],
     valueParser_literalAfterReference: ["Literal \"", /* literal */ "\" after reference to column \"", /* column */ "\"."],
-    valueParser_literalAfterClosing: ["Literal \"", /* literal */ "\" after closing parentheses."],
+    valueParser_literalAfterClosing: ["Literal \"", /* literal */ "\" after closing parenthesis."],
     valueParser_referenceAfterLiteral: ["Reference to column \"", /* column */ "\" after literal \"", /* literal */ "\"."],
     valueParser_referenceAfterReference: ["Reference to column \"", /* column */ "\" after reference to column \"", /* column */ "\"."],
-    valueParser_referenceAfterClosing: ["Reference to column \"", /* column */ "\" after closing parentheses."],
+    valueParser_referenceAfterClosing: ["Reference to column \"", /* column */ "\" after closing parenthesis."],
     valueParser_notAfterLiteral: ["Logical not operator \"", /* not */ "\" after literal \"", /* literal */ "\"."],
     valueParser_notAfterReference: ["Logical not operator \"", /* not */ "\" after reference to column \"", /* column */ "\"."],
-    valueParser_notAfterClosing: ["Logical not operator \"", /* not */ "\" after closing parentheses."],
+    valueParser_notAfterClosing: ["Logical not operator \"", /* not */ "\" after closing parenthesis."],
     valueParser_binaryAfterOperator: ["Binary operator \"", /* binary */ "\" after binary operator \"", /* binary */ "\"."],
-    valueParser_binaryAfterOpening: ["Binary operator \"", /* binary */ "\" after opening parentheses."],
-    valueParser_openingAfterLiteral: ["Opening parentheses after literal \"", /* literal */ "\"."],
-    valueParser_openingAfterReference: ["Opening parentheses after reference to column \"", /* column */ "\"."],
-    valueParser_openingAfterClosing: ["Opening parentheses after closing parentheses."],
-    valueParser_closingAfterOperator: ["Closing parentheses after binary operator \"", /* binary */ "\"."],
-    valueParser_closingAfterOpening: ["Closing parentheses after opening parentheses."],
+    valueParser_binaryAfterOpening: ["Binary operator \"", /* binary */ "\" after opening parenthesis."],
+    valueParser_openingAfterLiteral: ["Opening parenthesis after literal \"", /* literal */ "\"."],
+    valueParser_openingAfterReference: ["Opening parenthesis after reference to column \"", /* column */ "\"."],
+    valueParser_openingAfterClosing: ["Opening parenthesis after closing parenthesis."],
+    valueParser_closingAfterOperator: ["Closing parenthesis after binary operator \"", /* binary */ "\"."],
+    valueParser_closingAfterOpening: ["Closing parenthesis after opening parenthesis."],
 
-    stringUtils_missingClosingChar: ["Missing '", /* opening char */ "' after opening '", /* closing char */ "'."],
+    stringUtils_missingClosingChar: ["Missing '", /* closing char */ "' after opening '", /* opening char */ "'."],
     stringUtils_charNotFound: ["Expected \"", /* char */ "\" not found."],
 
-    renameNode_missingArrow: ["Invalid rename, use \"OldName -> NewName\" format separated by commas."],
-    renameNode_invalidNewName: ["Invalid rename to \"", /* name */
+    renameNode_missingArrow: ["Invalid renaming attempt, use the \"OldName -> NewName\" format separated by commas."],
+    renameNode_invalidNewName: ["Invalid renaming attempt to \"", /* name */
         "\". New column name must contain letters, numbers and underscores only and start with a letter or an underscore."],
-    renameNode_keywordNewName: ["Invalid rename to \"", /* name */ "\". New column name cannot be a keyword."],
-    renameNode_multipleRenameOfTheColumn: ["Multiple rename of column \"", /* name */ "\"."],
+    renameNode_keywordNewName: ["Invalid renaming attempt to \"", /* name */ "\". New column name cannot be a keyword."],
+    renameNode_multipleRenameOfTheColumn: ["Multiple renaming attempts of column \"", /* name */ "\"."],
 
     selectionNode_resultNotBoolean: ["Result of the selection condition ", /* condition */ " is not a boolean value, but ", /* type */ "."],
     thetaJoinNode_resultNotBoolean: ["Result of the theta join condition ", /* condition */ " is not a boolean value, but ", /* type */ "."],
 
     comparingOperator_differentInputTypes: ["Inputs for \"", /* operator */ "\"  have different types ", /* type */ " and ", /* type */ "."],
     computingOperator_inputTypesNotNumbers: ["Inputs for \"", /* operator */ "\" are not both numbers, they are ", /* type */ " and ", /* type */ "."],
-    logicalOperator_leftInputNotBoolean: ["Left input value for \"", /* operator */ "\" is not a boolean, but ", /* type */ "."],
-    logicalOperator_rightInputNotBoolean: ["Right input value for \"", /* operator */ "\" is not a boolean, but ", /* type */ "."]
+    logicalOperator_leftInputNotBoolean: ["Left-hand input value for \"", /* operator */ "\" is not a boolean, but ", /* type */ "."],
+    logicalOperator_rightInputNotBoolean: ["Right-hand input value for \"", /* operator */ "\" is not a boolean, but ", /* type */ "."]
 };
 
 export const EN: LanguageDef = {
@@ -127,7 +127,7 @@ export const EN: LanguageDef = {
         emptyColumn: "Column name cannot be empty",
         duplicitColumn: "Duplicit column name",
         keywordColumn: "Column name cannot be a keyword",
-        invalidColumn: "Invalid characters in column name",
+        invalidColumn: "Invalid characters in a column name",
 
         unsupportedNull: "Null values are not supported",
         invalidNumber: "Given string is not a number",
@@ -186,7 +186,7 @@ export const EN: LanguageDef = {
         loadButton: "Load",
         saveButton: "Save",
         samplesButton: "Samples",
-        samplesMenuTitle: "Prepared sample projects",
+        samplesMenuTitle: "Sample projects",
         settingsButton: "Settings",
         settingsNullValues: "Null values",
         settingsNullValuesAllowed: "allowed",
@@ -206,18 +206,18 @@ export const EN: LanguageDef = {
         loadAllButton: "Load all",
         loadAllButtonTooltip: "Loads all valid relation into the application",
         removeLoadedButton: "Remove loaded",
-        removeLoadedButtonTooltip: "Removes all loaded relations",
+        removeLoadedButtonTooltip: "Removes all the currently loaded relations",
         importButton: "Import",
         importButtonTooltip: "Adds new relations from files",
         exportButton: "Export",
-        exportButtonTooltip: "Saves stored relations to files",
+        exportButtonTooltip: "Saves all stored relations to files",
         loadButton: "Load",
-        loadButtonTooltip: "Loads the relation into the application",
+        loadButtonTooltip: "Loads the selected relation into the application",
         renameButton: "Rename",
         deleteButton: "Delete",
-        deleteButtonTooltip: "Deletes the relation",
+        deleteButtonTooltip: "Deletes the selected relation",
         revertButton: "Revert",
-        revertButtonTooltip: "Reverts to last loaded state"
+        revertButtonTooltip: "Reverts the selected relation to the last loaded state"
     },
 
     expressionSection: {
@@ -229,12 +229,12 @@ export const EN: LanguageDef = {
         exportButtonTooltip: "Saves expressions to a file",
 
         evaluateButton: "Evaluate",
-        evaluateButtonTooltip: "Evaluates selected RA expression",
+        evaluateButtonTooltip: "Evaluates the selected expression",
         renameButton: "Rename",
         deleteButton: "Delete",
-        deleteButtonTooltip: "Deletes selected RA expression",
+        deleteButtonTooltip: "Deletes the selected expression",
 
-        expressionTextareaPlaceholder: "Write RA expression here...",
+        expressionTextareaPlaceholder: "Write relational algebra expression here...",
 
         lineComment: "Line comment",
         blockComment: "Block comment"
@@ -244,14 +244,14 @@ export const EN: LanguageDef = {
         resultSectionHeader: "Result",
 
         exportEvalTreeButton: "Export",
-        exportEvalTreeButtonTooltip: "Saves the evaluation tree as a picture",
+        exportEvalTreeButtonTooltip: "Saves the evaluation tree as an image",
         evalTreeTitle: "Evaluation tree of",
 
         resultRelationTitle: "Result relation",
         intermediateRelationTitle: "Intermediate relation",
         addButton: "Add",
-        addButtonTooltip: "Adds the given relation to stored ones",
+        addButtonTooltip: "Adds the given relation between the stored ones",
         exportRelationButton: "Export",
-        exportRelationButtonTooltip: "Saves the given relation to a file",
+        exportRelationButtonTooltip: "Saves the selected relation to a file",
     }
 }
