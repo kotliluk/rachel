9a78bbb1d6398c20349daff70dbe917af3fcbbfb
Lukas Kotlik <kotliluk@fel.cvut.cz>
2021-10-02 11:31:34 +0200

    Add project.test.ts.

---
+++ src/project/tests/project.test.ts
@@ -0,0 +1,87 @@
+import {copyProject, isProjectObject, Project} from "../project";
+import {StoredRelation} from "../../relation/storedRelation";
+
+
+let project: Project = {
+  relations: [ StoredRelation.new('name', true).toDataObject() ],
+  expressions: [ { name: '', text: '' } ],
+  nullValuesSupport: true,
+}
+
+beforeEach(() => {
+  project = {
+    relations: [ StoredRelation.new('name', true).toDataObject() ],
+    expressions: [ { name: '', text: '' } ],
+    nullValuesSupport: true,
+  }
+});
+
+const errMsg1 = 'The file cannot be parsed to an object.';
+const errMsg2 = 'The file must contain the field "relations: StoredRelationData[]" of length at least one';
+const errMsg3 = 'The file must contain the field "expressions: Expression[]" of length at least one';
+const errMsg4 = 'The file must contain the field "nullValuesSupport: boolean"';
+const okMsg = 'OK';
+
+describe('isProjectObject', () => {
+  test.each([
+    { obj: undefined, expected: errMsg1 },
+    { obj: 'str', expected: errMsg1 },
+    { obj: {}, expected: errMsg2 },
+    { obj: { relations: project.relations }, expected: errMsg3 },
+    { obj: { relations: project.relations, expressions: project.expressions }, expected: errMsg4 },
+    {
+      obj: { relations: project.relations, expressions: project.expressions, nullValuesSupport: project.nullValuesSupport },
+      expected: okMsg,
+    },
+  ])('%s', ({ obj, expected }) => {
+    // act
+    const actual = isProjectObject(obj);
+    // assert
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('copyProject', () => {
+  test('copy has the same fields', () => {
+    // arrange
+    const original = project;
+    // act
+    const copy = copyProject(original);
+    // assert
+    expect(copy).toStrictEqual(original);
+  });
+
+  test('changes of copy does not affect the original', () => {
+    // arrange
+    const original = project;
+    const originalNVS = original.nullValuesSupport;
+    const originalRelationsCount = original.relations.length;
+    const originalExpressionsCount = original.expressions.length;
+    const copy = copyProject(original);
+    // act
+    copy.nullValuesSupport = !copy.nullValuesSupport;
+    copy.relations = [];
+    copy.expressions = [];
+    // assert
+    expect(original.nullValuesSupport).toBe(originalNVS);
+    expect(original.relations.length).toBe(originalRelationsCount);
+    expect(original.expressions.length).toBe(originalExpressionsCount);
+  });
+
+  test('changes of original does not affect the copy', () => {
+    // arrange
+    const original = project;
+    const copy = copyProject(original);
+    const copyNVS = copy.nullValuesSupport;
+    const copyRelationsCount = copy.relations.length;
+    const copyExpressionsCount = copy.expressions.length;
+    // act
+    original.nullValuesSupport = !original.nullValuesSupport;
+    original.relations = [];
+    original.expressions = [];
+    // assert
+    expect(copy.nullValuesSupport).toBe(copyNVS);
+    expect(copy.relations.length).toBe(copyRelationsCount);
+    expect(copy.expressions.length).toBe(copyExpressionsCount);
+  });
+});
