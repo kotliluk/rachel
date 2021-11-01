1ba1da8a7fb77bb09d1e0515baf4127f616b201f
Lukas Kotlik <kotliluk@fel.cvut.cz>
2021-11-01 15:31:41 +0100

    Automate generating of expected testing results.

--- test_data/computeLogical/boundaryValuesGenerator.js
+++ test_data/computeLogical/boundaryValuesGenerator.js
@@ -1,6 +1,68 @@
 const fs = require('fs')
 
 
+const expectedResults = [
+  // type = and
+  [
+    ['error1', 'error1', 'error1', 'error3'],
+    ['error2', 'true', 'false', 'error3'],
+    ['error2', 'false', 'false', 'error3'],
+    ['error2', 'false', 'false', 'error3'],
+  ],
+  // type = or
+  [
+    ['error1', 'error1', 'error1', 'error3'],
+    ['error2', 'true', 'true', 'error3'],
+    ['error2', 'true', 'false', 'error3'],
+    ['error2', 'true', 'false', 'error3'],
+  ],
+  // type = not
+  [
+    ['error4', 'error4', 'error4', 'error1'],
+    ['error4', 'error4', 'error4', 'false'],
+    ['error4', 'error4', 'error4', 'true'],
+    ['error4', 'error4', 'error4', 'false'],
+  ],
+]
+
+// maps params to their index in expectedResult
+const getExpectedResultIndex = (str) => {
+  switch (str) {
+    case 'and':
+      return 0
+    case 'or':
+      return 1
+    case 'not':
+      return 2
+
+    case 'aNumStr':
+      return 0
+    case 'aTrue':
+      return 1
+    case 'aFalse':
+      return 2
+    case 'aNull':
+      return 3
+
+    case 'bNumStr':
+      return 0
+    case 'bTrue':
+      return 1
+    case 'bFalseNull':
+      return 2
+    case 'bUndef':
+      return 3
+
+    default:
+      return 5
+  }
+}
+
+// gets expected result for the given parameter values
+const getExpectedResult = (params) => {
+  return expectedResults[getExpectedResultIndex(params[0])][getExpectedResultIndex(params[1])][getExpectedResultIndex(params[2])]
+}
+
 // boundary values for each EC of a parameter
 const aParamMap = new Map([
   ['aNumStr', ['1', 'str']],
@@ -8,6 +70,7 @@ const aParamMap = new Map([
   ['aFalse', ['false']],
   ['aNull', ['null']],
 ])
+
 // boundary values for each EC of b parameter
 const bParamMap = new Map([
   ['bNumStr', ['1', 'str']],
@@ -16,23 +79,26 @@ const bParamMap = new Map([
   ['bUndef', ['undefined']],
 ])
 
-const resultLines = []
-
 const processOrig = (orig, result) => {
+  const resultLines = []
+
   // loads original file
   const origLines = fs.readFileSync(orig,'utf8').split('\n')
 
   // adds param names line
   resultLines.push(origLines[0] + ',expected')
 
-  origLines.slice(1).forEach(line => {
+  origLines.slice(1)
+    .filter(line => line.trim().length > 4)
+    .forEach(line => {
     // parses the line into parameters
     const params = line.split(',')
     const aMappings = aParamMap.get(params[1]) || []
     const bMappings = bParamMap.get(params[2]) || []
+    const expected = getExpectedResult(params)
     // adds boundary values for parameters
     aMappings.forEach(n => {
-      bMappings.forEach(m => resultLines.push(params[0] + ',' + n + ',' + m))
+      bMappings.forEach(m => resultLines.push(params[0] + ',' + n + ',' + m + ',' + expected))
     })
   })
 
